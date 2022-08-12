cd9d6184a0fa36876a059824cc9bdb493b27e965
hugcis <hmj.cisneros@gmail.com>
2022-08-12 10:31:00 +0200

    Refactor run_2d.py to use load_weights method

--- run_2d.py
+++ run_2d.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-import sys
 import pickle
+import sys
 
-import numpy as np
 import torch
-import torch.nn as nn
 
 from evo_ca.models import NCAModel
-from evo_ca.utils import spawn_state_nca, get_state_nca, CLIENT, Blocks
+from evo_ca.utils import get_state_nca, spawn_state_nca, Blocks, CLIENT
 
-STEPS=2
-
-ca = NCAModel()
-fname = sys.argv[1]
-weights = pickle.load(open(fname, "rb"))
-
-list(ca.dmodel.children())[1].weight = nn.Parameter(
-    torch.moveaxis(torch.Tensor(weights[0]), (2, 3), (1, 0)))
-list(ca.dmodel.children())[1].bias = nn.Parameter(torch.Tensor(weights[1]))
-
-list(ca.dmodel.children())[3].weight = nn.Parameter(
-    torch.moveaxis(torch.Tensor(weights[2]), (2, 3), (1, 0)))
-list(ca.dmodel.children())[3].bias = nn.Parameter(torch.Tensor(weights[3]))
+STEPS = 2
 
 
-arr, s_blocks = get_state_nca((70, 1, 70))
-CLIENT.spawnBlocks(Blocks(blocks=s_blocks.blocks))
+def main() -> None:
+    ca = NCAModel()
+    fname = sys.argv[1]
 
-x = torch.Tensor(arr)
-while True:
-    for t in range(STEPS):
-        x = ca(x)
-    spawn_state_nca(x[0, :4, :, :])
+    with open(fname, "rb") as f:
+        weights = pickle.load(f)
+
+    ca.load_weights(weights)
+
+    arr, s_blocks = get_state_nca((70, 1, 70))
+    CLIENT.spawnBlocks(Blocks(blocks=s_blocks.blocks))
+
+    x = torch.tensor(arr, dtype=torch.float32)
+    while True:
+        for _ in range(STEPS):
+            x = ca(x)
+        spawn_state_nca(x[0, :4, :, :])
+
+
+if __name__ == "__main__":
+    main()
