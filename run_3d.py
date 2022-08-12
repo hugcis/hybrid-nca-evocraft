5ac20d86a23f65889e0ee1dc3f89676442bde0cb
hugcis <hmj.cisneros@gmail.com>
2022-08-12 10:31:00 +0200

    Refactor run_3d.py to use load_weights method

--- run_3d.py
+++ run_3d.py
@@ -1,46 +1,50 @@
 #!/usr/bin/env python3
-import sys
 import pickle
+import sys
 
 import numpy as np
+import numpy.typing as npt
 import torch
-import torch.nn as nn
 
 from evo_ca.models import NCAModel3D
 from evo_ca.utils import spawn_state_nca_3d
 
 STEPS = 4
 
-ca3d = NCAModel3D()
-fname = sys.argv[1]
-weights = pickle.load(open(fname, "rb"))
 
-list(ca3d.dmodel.children())[0].weight = nn.Parameter(
-    torch.moveaxis(torch.Tensor(weights[0]), (3, 4), (1, 0)))
+def create_seed(size: int = 32, channels: int = 16) -> npt.NDArray[np.float64]:
+    """Create initial seed state for the 3D NCA."""
+    seed = np.zeros([2, size, size, size, channels], dtype=np.float64)
+    h, d = size, size
+    w = 10
 
-list(ca3d.dmodel.children())[1].weight = nn.Parameter(
-    torch.moveaxis(torch.Tensor(weights[1]), (3, 4), (1, 0)))
-list(ca3d.dmodel.children())[1].bias = nn.Parameter(torch.Tensor(weights[2]))
+    seed[0, h // 2, w // 2 - 4 : w // 2 - 1, d // 2 + 3, 3:] = 1.0
+    seed[1, h // 2, w // 2 + 1, d // 2 - 5, 3:] = 1.0
+    seed[1, h // 2, w // 2 + 1, d // 2 - 7, 3:] = 1.0
+    seed[1, h // 2, w // 2, d // 2 - 2 : d // 2 + 1, 3:] = 1.0
+    seed[0, h // 2, w // 2 + 3, d // 2 - 6, 3:] = 1.0
 
-list(ca3d.dmodel.children())[3].weight = nn.Parameter(
-    torch.moveaxis(torch.Tensor(weights[3]), (3, 4), (1, 0)))
-list(ca3d.dmodel.children())[3].bias = nn.Parameter(torch.Tensor(weights[4]))
+    return seed
 
-s = 32
-h, d = s, s
-seed = np.zeros([2, s, s, s, 16], dtype=np.float)
-w = 10
-seed[0, h//2, w//2 - 4:w//2 - 1, d//2 + 3, 3:] = 1.0
-seed[1, h//2, w//2 + 1, d//2 - 5, 3:] = 1.0
-seed[1, h//2, w//2 + 1, d//2 - 7, 3:] = 1.0
 
-seed[1, h//2, w//2, d//2 - 2:d//2 + 1, 3:] = 1.0
-seed[0, h//2, w//2 + 3, d//2 - 6, 3:] = 1.0
+def main() -> None:
+    ca3d = NCAModel3D()
+    fname = sys.argv[1]
 
-x = torch.Tensor(seed[:])
-x = torch.moveaxis(x, -1, 1)
+    with open(fname, "rb") as f:
+        weights = pickle.load(f)
 
-while True:
-    for _ in range(STEPS):
-        x = ca3d(x)
-    spawn_state_nca_3d(x[1, :4, :, :, :], z_offset=-150, x_offset=100, y_base=5)
+    ca3d.load_weights(weights)
+
+    seed = create_seed()
+    x = torch.tensor(seed, dtype=torch.float32)
+    x = x.permute(0, 4, 1, 2, 3)  # Move channels to dim 1
+
+    while True:
+        for _ in range(STEPS):
+            x = ca3d(x)
+        spawn_state_nca_3d(x[1, :4, :, :, :], z_offset=-150, x_offset=100, y_base=5)
+
+
+if __name__ == "__main__":
+    main()
